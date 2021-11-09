74725c82a3ad217b0a57274ffdad79078eee3cd0
codeByCandlelight <Terryltilley@gmail.com>
2021-11-08 16:49:46 -0800

    Finished exercise

--- src/app/warningalert/warningalert.component.ts
+++ src/app/warningalert/warningalert.component.ts
@@ -3,7 +3,14 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-warningalert",
   templateUrl: "./warningalert.component.html",
-  styleUrls: ["./warningalert.component.css"],
+  styles: [`
+  * {
+    padding: 20px;
+    background-color: pink;
+    color: red;
+    border: 1px solid black; 
+  }
+  `],
 })
 export class WarningalertComponent {
 
